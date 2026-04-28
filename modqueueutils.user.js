7f26032c7da6b94a9333c4681e2b143c7c33368d
ComReq <tele.didar.lol@gmail.com>
2026-04-28 11:02:42 +0500

    a

--- modqueueutils.user.js
+++ modqueueutils.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         mod queue utils
 // @namespace    http://tampermonkey.net/
-// @version      2
+// @version      whatever
 // @description  in the modqueue
 // @author       commentar reqeust
 // @match        https://danbooru.donmai.us/modqueue*
@@ -40,7 +40,30 @@ function handleError(xhr, status, error)
     Danbooru.error("Network error");
 }
 
-function check(e, preview)
+function checkPost(resp)
+{
+    let aiGen = false;
+    let aiAssist = false;
+    for (const post of resp) {
+        if (post.tag_string_meta.includes("ai-generated") && !post.tag_string_meta.includes("ai-generated_background")) {
+            aiGen = true;
+            break;
+        } else if (post.tag_string_meta.includes("ai-assisted") && !post.tag_string_meta.includes("ai-generated_background")) {
+            aiAssist = true;
+            break;
+        }
+    }
+
+    if (aiGen) {
+        Danbooru.error("AI-generated found!");
+    } else if (aiAssist) {
+        Danbooru.error("AI-assisted found!");
+    } else {
+        Danbooru.notice("ok");
+    }
+}
+
+function clickCheck(e, preview)
 {
     e.target.setAttribute("disabled", "disabled");
     e.target.innerText = "oke wait";
@@ -51,28 +74,16 @@ function check(e, preview)
         method: "GET",
         success: resp => {
             const arttag = resp.tag_string_artist;
+            if (!arttag) {
+                Danbooru.error("No artist tag!");
+                resetButton(e.target);
+                return;
+            }
             $.ajax({
                 url: `/posts.json?tags=${arttag}`,
                 method: "GET",
                 success: resp => {
-                    let aiGen = false;
-                    let aiAssist = false;
-                    for (const post of resp) {
-                        if (post.tag_string_meta.includes("ai-generated") && !post.tag_string_meta.includes("ai-generated_background")) {
-                            aiGen = true;
-                            break;
-                        } else if (post.tag_string_meta.includes("ai-assisted") && !post.tag_string_meta.includes("ai-generated_background")) {
-                            aiAssist = true;
-                            break;
-                        }
-                    }
-                    if (aiGen) {
-                        Danbooru.error("AI-generated found!");
-                    } else if (aiAssist) {
-                        Danbooru.error("AI-assisted found!");
-                    } else {
-                        Danbooru.notice("ok");
-                    }
+                    checkPost(resp);
                     resetButton(e.target);
                 },
                 error: (xhr, status, error) => {
@@ -95,7 +106,7 @@ function aiCheckButton()
         const button = document.createElement("a");
         button.classList.add("button-primary", "button-xs", "chip-yellow");
         button.innerText = "Check";
-        button.addEventListener("click", e => check(e, p));
+        button.addEventListener("click", e => clickCheck(e, p));
         d.appendChild(button);
     });
 }
