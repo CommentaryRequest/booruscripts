6a77c969236950d49fa9e3634922d3b2a90f95f7
ComReq <tele.didar.lol@gmail.com>
2026-08-11 18:46:57 +0500

    Create absolutetime.user.js

---
+++ absolutetime.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Absolute Time
+// @namespace    http://tampermonkey.net/
+// @version      1
+// @description  Danbooru userscript for displaying absolute time in places where it's relative.
+// @author       commentary request
+// @match        *://*.donmai.us/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=donmai.us
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const times = document.querySelectorAll("time");
+    times.forEach(time => {
+        const date = time.title;
+        time.innerText += " (" + date + ")";
+    });
+})();
